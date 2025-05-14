7aa369a54774a7f1f842898c50165bc0909d1c79
SIDANPillow <ansonchui2608@gmail.com>
2025-05-14 13:28:09 +0800

    fix: configure admin wallet list to be env

--- src/data/admins.ts
+++ src/data/admins.ts
@@ -2,17 +2,19 @@ export type AdminUser = {
   wallet_address: string;
 };
 
-export const adminAccessList: AdminUser[] = [
-  {
-    wallet_address:
-      "addr_test1qrtu8zzz00devcdfmygzr4fyd9l890tuhgv9d5ckg93khghl9j9q3klulzrhcpr3acpdefq5ue80yzeqkckllkn5lh4setgyl6",
-  },
-  {
-    wallet_address:
-      "addr_test1qztdn7qwa2nv2mnre2leyk3xz0cnhag3q2ktth6ektvnqrylyvks5wjuknwd5r9pfa204s8ynqnxxf88wsx3nc5k8xvsm568tf",
-  },
-  {
-    wallet_address:
-      "addr_test1qqlvh5p0z05wr48vzgh6t3n0mmm34rhfju2heduqqextxmpmhecxmjdm0k5gvpvaau9gvgae0ct8se07j0d9x5rkw40sungk4s",
-  },
-];
+// Read admin wallet addresses from environment variables
+export const adminAccessList: AdminUser[] = [];
+
+// Parse the comma-separated list of wallet addresses
+const adminWallets = process.env.ADMIN_WALLET_ADDRESSES;
+if (adminWallets) {
+  const walletAddresses = adminWallets.split(";");
+
+  walletAddresses.forEach((address) => {
+    if (address.trim()) {
+      adminAccessList.push({
+        wallet_address: address.trim(),
+      });
+    }
+  });
+}
